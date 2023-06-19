81470ae363b818878e2ee547401447ba004be067
Emma <em.lebedeva85@gmail.com>
2023-06-19 18:46:15 +0300

    Update Task62

--- Task62/Program.cs
+++ Task62/Program.cs
@@ -4,3 +4,32 @@
 // 12 13 14 05
 // 11 16 15 06
 // 10 09 08 07
+
+
+int[,] array = new int[4, 4];
+int firstNumber = 1;
+for (int i = 0; i < 4; i++)
+{
+    array[i, 0] = firstNumber;
+    firstNumber++;
+
+    for (int j = 0; j < 4; j++)
+    {
+        array[0, j+1] = firstNumber;
+        firstNumber++;
+    }
+}
+
+void PrintArray2d(int[,] array2d)
+{
+    for (int i = 0; i < array2d.GetLength(0); i++)
+    {
+        for (int j = 0; j < array2d.GetLength(1); j++)
+        {
+            Console.Write(array2d[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+PrintArray2d(array);
